b13549f4398f864aaa198025516c197a217361ef
Akilo <sergeencowgame@yandex.ru>
2024-02-10 16:55:45 +0300

    first

--- questionnaire/admin.py
+++ questionnaire/admin.py
@@ -1,10 +1,34 @@
 from django.contrib import admin
 from .models import Competence, User, Question,UserAnswer,Group
 
-admin.site.register(Competence)
-admin.site.register(User)
-admin.site.register(UserAnswer)
-admin.site.register(Question)
-admin.site.register(Group)
+from django.contrib import admin
+from .models import User, Group, Competence, Question, UserAnswer
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'full_name', 'chosen_group')
+    list_filter = ('chosen_group',)
+    search_fields = ('full_name',)
+
+
 class GroupAdmin(admin.ModelAdmin):
-    list_display = ('title', 'show_results')
+    list_display = ('id', 'title', 'show_results')
+
+
+class CompetenceAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'text')
+
+
+class UserAnswerAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'group', 'competence', 'date')
+
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Group, GroupAdmin)
+admin.site.register(Competence, CompetenceAdmin)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(UserAnswer, UserAnswerAdmin)
