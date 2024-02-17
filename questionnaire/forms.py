3506e75e2eec7409be7d7ab848ba92a2b95de074
Akilo <sergeencowgame@yandex.ru>
2024-02-18 00:27:36 +0300

    second

--- questionnaire/forms.py
+++ questionnaire/forms.py
@@ -5,16 +5,40 @@ from .models import Group, Competence, Question
 class GroupForm(forms.ModelForm):
     class Meta:
         model = Group
-        fields = ['title', 'users', 'show_results','num_competences_displayed']
+        fields = ['title', 'users', 'show_results', 'num_competences_displayed']
+        labels = {
+            'title': 'Название',
+            'users': 'Пользователи',
+            'show_results': 'Показывать результаты',
+            'num_competences_displayed': 'Количество отображаемых компетенций'
+        }
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'})
+        }
 
 
 class CompetenceForm(forms.ModelForm):
     class Meta:
         model = Competence
         fields = ['name', 'questions']
+        labels = {
+            'name': 'Название',
+            'questions': 'Вопросы',
+        }
+
+
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'})
+        }
 
 
 class QuestionForm(forms.ModelForm):
     class Meta:
         model = Question
         fields = ['text']
+        labels = {
+            'text': 'Текст'
+        }
+        widgets = {
+            'text': forms.TextInput(attrs={'class': 'form-control'})
+        }
